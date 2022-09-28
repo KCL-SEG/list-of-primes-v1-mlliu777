4f14fbadc1364295cda05ef377b6b44b53f5a2d4
Manlin Liu <manlin.liu@kcl.ac.uk>
2022-09-28 23:07:47 +0100

    Complete tast of printing prime numbers

--- primes.py
+++ primes.py
@@ -2,5 +2,22 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
+    list = [2]
+    count = 2
+    for number in range(1, number_of_primes):
+        while len(list) <= number:
+            count += 1
+            status = False
+            for prime in list:
+                if count % prime == 0:
+                    status = True
+                    break
+                else:
+                    continue
+            if status:
+                continue
+            else:
+                list.append(count)
+                break
+
     return list
